0f8403979f670febe1b5300c10a50ffaf390491a
Epro <Praktikant.Epro@emerson.com>
2021-10-19 10:50:38 +0200

    Updated Status Bar and created Information Service

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InformationService } from '../information.service';
 
 @Component({
   selector: 'app-status',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatusComponent implements OnInit {
 
-  constructor() { }
-
+  private _valueFlags = "000";
+  private _valueTime = "000";
+  
+  constructor(private infoService : InformationService) { }
+  
   ngOnInit(): void {
   }
 
+
+  
+  public get valueFlags() {
+    return this._valueFlags;
+  }
+  public set valueFlags(value) {
+    this._valueFlags = value;
+  }
+  public get valueTime() {
+    return this._valueTime;
+  }
+  public set valueTime(value) {
+    this._valueTime = value;
+  }
 }
